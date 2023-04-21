1f563fd9e88bb15dd78bcc6b88bb16d8d5b4c9d1
am.yazdanmanesh <amir.yazdanmanesh@gmail.com>
2023-04-21 15:50:16 +0330

    Add ViewModelScope annotation. By using this annotation, you can ensure that each ViewModel instance is created once and is reused throughout the lifetime of the component.

---
+++ core/src/main/java/utils/ViewModelScope.java
@@ -0,0 +1,13 @@
+package utils;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+import javax.inject.Scope;
+
+@Scope
+@Documented
+@Retention(RUNTIME)
+public @interface ViewModelScope {}
