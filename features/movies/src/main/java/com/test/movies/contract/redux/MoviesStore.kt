6a2ccba6a3b7602e080cdd84e86fa31798fc58eb
am.yazdanmanesh <amir.yazdanmanesh@gmail.com>
2023-04-21 13:07:01 +0330

    Add movies store to holds the application state and provides a way to access and update it

---
+++ features/movies/src/main/java/com/test/movies/contract/redux/MoviesStore.kt
@@ -0,0 +1,16 @@
+package com.test.movies.contract.redux
+
+import com.test.core.base.BaseStore
+import com.test.movies.contract.MoviesAction
+import com.test.movies.contract.MoviesState
+import javax.inject.Inject
+
+open class MoviesStore @Inject constructor(
+    moviesMiddleware: MoviesBaseMiddleware
+): BaseStore<MoviesState, MoviesAction>(
+    initialState = MoviesState.initialize(),
+    reducer = MoviesReducer(),
+    middlewares = listOf(
+        moviesMiddleware
+    )
+)
