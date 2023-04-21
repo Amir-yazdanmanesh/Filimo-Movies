7359b58bd0c5711f692f265488346483c91671f3
am.yazdanmanesh <amir.yazdanmanesh@gmail.com>
2023-04-21 15:48:47 +0330

    Add ViewModelKey,By using this annotation in combination with the @IntoMap and @ViewModelProvider annotations, you can ensure that each ViewModel has its own provider that can be injected into other classes as needed.

---
+++ core/src/main/java/utils/ViewModelKey.kt
@@ -0,0 +1,11 @@
+package utils
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+//you can ensure that each ViewModel has its own provider that can be injected into other classes as needed.
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
