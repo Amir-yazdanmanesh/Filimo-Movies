2c13628b83154375d98c765edaea662f39a156ff
am.yazdanmanesh <amir.yazdanmanesh@gmail.com>
2023-04-21 23:42:03 +0330

    Fix EpoxyDataBindingConfig import path

--- features/movies/src/main/java/com/test/movies/util/EpoxyDataBindingConfig.java
+++ features/movies/src/main/java/com/test/movies/util/EpoxyDataBindingConfig.java
@@ -1,7 +1,7 @@
 package com.test.movies.util;
 
 import com.airbnb.epoxy.EpoxyDataBindingPattern;
-import com.test.filimo.R;
+import com.test.movies.R;
 
 @EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "view_holder")
 interface EpoxyDataBindingConfig {}
