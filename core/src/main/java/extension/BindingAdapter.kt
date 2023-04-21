0a9c8bcab5b5bd1cfa97866dd0187f3c1f42e866
am.yazdanmanesh <amir.yazdanmanesh@gmail.com>
2023-04-21 17:07:28 +0330

    Add shimmer effect to the recycler view in the main activity layout. The shimmer effect is displayed while data is being loaded from the server.

---
+++ core/src/main/java/extension/BindingAdapter.kt
@@ -0,0 +1,13 @@
+package extension
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("loadImage")
+fun loadImage(imageView: ImageView, imageUrl: String?) {
+    imageUrl?.let{
+        Glide.with(imageView.context)
+            .load(imageUrl).into(imageView)
+    }
+}
