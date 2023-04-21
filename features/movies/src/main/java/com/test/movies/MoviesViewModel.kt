34b51f7900237f5d05a85443354617dd93bb75b6
am.yazdanmanesh <amir.yazdanmanesh@gmail.com>
2023-04-21 17:35:31 +0330

    feat: Add getMoviesEvent method to MoviesViewModel

--- features/movies/src/main/java/com/test/movies/MoviesViewModel.kt
+++ features/movies/src/main/java/com/test/movies/MoviesViewModel.kt
@@ -1,9 +1,11 @@
 package com.test.movies
 
+import androidx.lifecycle.viewModelScope
 import com.test.core.base.BaseViewModelRedux
 import com.test.movies.contract.MoviesAction
 import com.test.movies.contract.MoviesState
 import com.test.movies.contract.redux.MoviesStore
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MoviesViewModel @Inject constructor(
@@ -11,5 +13,12 @@ class MoviesViewModel @Inject constructor(
 ) : BaseViewModelRedux<MoviesState, MoviesAction>(store) {
     override val initialState: MoviesState = MoviesState.initialize()
 
+    init {
+        getMoviesEvent(MoviesState.noSearch)
+    }
+
+    fun getMoviesEvent(query: String) = viewModelScope.launch {
+        store.dispatch(MoviesAction.GetMovies(query))
+    }
 
 }
